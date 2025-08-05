544404df7961c28f16d906a891b6af848bef917a
OWOEOMO <50674595+OWOEOMO@users.noreply.github.com>
2025-08-05 13:59:39 -0700

    Create __init__.py

---
+++ python/train/__init__.py
@@ -0,0 +1,6 @@
+"""
+Training subpackage.
+
+This package contains scripts for training agents in Shattered Pixel Dungeon,
+including unsupervised RND pretraining and supervised fine-tuning with extrinsic rewards.
+"""
